22ccbcbf4b53293f1a8c97da5b32a05162bb7f33
noah <noahgray@me.com>
2015-03-26 16:56:10 -0700

    Decompress pages
    Decompress pages with Needle before scraping.

--- scrape.js
+++ scrape.js
@@ -2,7 +2,8 @@ var path = require("path");
 var Promise = require("bluebird");
 var objectAssign = require("object-assign"),
 	jsdom = require( 'jsdom' ),
-	fs = require( 'fs' )
+	fs = require( 'fs' ),
+	needle = require( 'needle' )
 
 var readabilityPath = process.env.READABILITY_LIB_PATH ||
                       path.normalize(path.join(__dirname, "vendor", "Readability.js"));
@@ -13,31 +14,37 @@ module.exports = function scrape(url, options) {
   options = options || {};
   if (!url) throw new Error("Missing url.");
   return new Promise(function(fulfill, reject) {
-    if (options.userAgent) {
-      childArgs.push(options.userAgent);
-    }
-    var webpage = jsdom.env({
-		url: url,
-		src: [ readabilityScript ],
-		done: function ( error, window ) {
-			if ( error ) return reject( error )
-			
-			var location = window.location
-			
-			var readerArgs = {
-				spec: location.href,
-    			host: location.host,
-    			prePath: location.protocol + "//" + location.host, // TODO This is incomplete, needs username/password and port
-    			scheme: location.protocol.substr(0, location.protocol.indexOf(":")),
-    			pathBase: location.protocol + "//" + location.host + 		location.pathname.substr(0, location.pathname.lastIndexOf("/") + 1)
+	var data = needle.get( url, { 
+		compressed: true,
+		follow_max: 5
+	}, function ( error, response ) {
+		if ( error ) return reject( error )
+		
+		console.error( response )
+		
+		jsdom.env({
+			html: response.body,
+			src: [ readabilityScript ],
+			done: function ( error, window ) {
+				if ( error ) return reject( error )
+
+				var location = window.location
+
+				var readerArgs = {
+					spec: location.href,
+					host: location.host,
+					prePath: location.protocol + "//" + location.host, // TODO This is incomplete, needs username/password and port
+					scheme: location.protocol.substr(0, location.protocol.indexOf(":")),
+					pathBase: location.protocol + "//" + location.host + 		location.pathname.substr(0, location.pathname.lastIndexOf("/") + 1)
+				}
+
+				var article = new window.Readability( readerArgs, window.document ).parse()
+
+				window.close()
+
+				fulfill( article )
 			}
-			
-			var article = new window.Readability( readerArgs, window.document ).parse()
-			
-			window.close()
-				
-			fulfill( article )
-		}
+		})
 	})
   });
 };
