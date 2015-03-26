27d6acdd3d1215cefd8a70ec7d1832da30f872b5
noah <noahgray@me.com>
2015-03-26 13:24:42 -0700

    Replaces PhantomJS with JSDOM
    Attempts to improve performance by replacing PhantomJS with JSDOM.

--- scrape.js
+++ scrape.js
@@ -1,45 +1,43 @@
-var childProcess = require("child_process");
-var phantomjs = require("phantomjs");
-var binPath = phantomjs.path;
 var path = require("path");
 var Promise = require("bluebird");
-var objectAssign = require("object-assign");
+var objectAssign = require("object-assign"),
+	jsdom = require( 'jsdom' ),
+	fs = require( 'fs' )
 
 var readabilityPath = process.env.READABILITY_LIB_PATH ||
                       path.normalize(path.join(__dirname, "vendor", "Readability.js"));
+
+var readabilityScript = fs.readFileSync( readabilityPath, 'utf-8' )
 
 module.exports = function scrape(url, options) {
   options = options || {};
   if (!url) throw new Error("Missing url.");
   return new Promise(function(fulfill, reject) {
-    var childArgs = [path.join(__dirname, "phantom-scrape.js"), url, readabilityPath];
     if (options.userAgent) {
       childArgs.push(options.userAgent);
     }
-    childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
-      if (err) {
-        return reject(err);
-      }
-      var response, error;
-      try {
-        response = JSON.parse(stdout);
-      } catch (e) {
-        error = {
-          message: "Unable to parse JSON proxy response.",
-          line: e.line,
-          stack: e.stack
-        };
-      }
-      if (response && response.error) {
-        error = response.error;
-      }
-      if (error) {
-        reject(objectAssign(new Error(error.message), error));
-      } else if (!response) {
-        reject(new Error("Empty scraped response."));
-      } else {
-        fulfill(response);
-      }
-    });
+    var webpage = jsdom.env({
+		url: url,
+		src: [ readabilityScript ],
+		done: function ( error, window ) {
+			if ( error ) return reject( error )
+			
+			var location = window.location
+			
+			var readerArgs = {
+				spec: location.href,
+    			host: location.host,
+    			prePath: location.protocol + "//" + location.host, // TODO This is incomplete, needs username/password and port
+    			scheme: location.protocol.substr(0, location.protocol.indexOf(":")),
+    			pathBase: location.protocol + "//" + location.host + 		location.pathname.substr(0, location.pathname.lastIndexOf("/") + 1)
+			}
+			
+			var article = new window.Readability( readerArgs, window.document ).parse()
+			
+			window.close()
+				
+			fulfill( article )
+		}
+	})
   });
 };
